befd0e59a4e7903e1ce5318854ffa3b8eec07622
Yogesh <yogeshmanthanitsolution@gmail.com>
2024-12-25 10:22:09 +0530

    index page path change

---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+
+
+
+const Header = () => {
+    return(
+        <div className="header-container">
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header; 
